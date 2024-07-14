21a0c5adaeffe6d62f7b609a17061daa179660ce
YukeChan2002 <yukeshchandran002@gmail.com>
2024-07-14 12:42:36 +0530

    Third Commit

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,63 +4,65 @@ import Document, {
   Main,
   NextScript,
   DocumentContext,
-  DocumentInitialProps
-} from 'next/document'
-import { ServerStyleSheet } from 'styled-components'
+  DocumentInitialProps,
+} from "next/document";
+import { ServerStyleSheet } from "styled-components";
 
 const meta = {
-  title: 'Evander Inácio - Desenvolvedor Front-End',
+  title: "Yukesh Chandran - Full Stack Developer",
   description:
-    'Desenvolvedor Front-end do Brasil. Especializado em criação de interfaces e funcionalidades para aplicativos da web usando React.js e Next.js.',
-  image:
-    'https://raw.githubusercontent.com/EvanderInacio/Portfolio/main/public/ogimage.png'
-}
+    "Full Stack Developer. Specializing in creating interfaces and functionalities for web applications using React.js.",
+  image: "#",
+};
 
 export default class MyDocument extends Document {
-  static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
-		const sheet = new ServerStyleSheet();
-		const originalRenderPage = ctx.renderPage;
+  static async getInitialProps(
+    ctx: DocumentContext
+  ): Promise<DocumentInitialProps> {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
 
-		try {
-			ctx.renderPage = () =>
-				originalRenderPage({
-					enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
-				});
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        });
 
-			const initialProps = await Document.getInitialProps(ctx);
-			return {
-				...initialProps,
-				styles: [initialProps.styles, sheet.getStyleElement()],
-			};
-		} finally {
-			sheet.seal();
-		}
-	}
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: [initialProps.styles, sheet.getStyleElement()],
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
 
   render() {
     return (
-      <Html lang="pt-BR">
+      <Html lang="en">
         <Head>
           <meta charSet="utf-8" />
-          <meta name="author" content="Evander Inácio" />
+          <meta name="author" content="Yukesh Chandran" />
           <meta name="description" content={meta.description} />
           <meta itemProp="name" content={meta.title} />
           <meta itemProp="description" content={meta.description} />
           <meta itemProp="image" content={meta.image} />
           <meta
             name="keywords"
-            content="Evander, Evander Inacio, desenvolvedor, website, programador, front-end, personal website, developer, portfolio, sites, web, são paulo, sp, JavaScript, TypeScript, ReactJS, NextJS, software, aplicações, Freelancer, portfolio developer, e-commerce, Evander portfolio"
+            content="Yukesh, Yukesh Chandran, developer, website, programmer, front-end, back-end,personal website, developer, portfolio, websites, web, São Paulo, SP, JavaScript, ReactJS, NodeJS, software, applications, Freelancer, portfolio developer, e-commerce, Yukesh portfolio"
           />
 
           <meta name="theme-color" content="#00d9ff" />
-          <meta name="copyright" content="Evander Inácio 2023" />
-          <meta http-equiv="content-language" content="pt-br" />
+          <meta name="copyright" content="Yukesh Chandran 2024" />
+          <meta httpEquiv="content-language" content="en" />
           <meta name="robots" content="index, follow" />
-          <meta http-equiv="cache-control" content="no-cache" />
-          <meta http-equiv="pragma" content="no-cache" />
-          <meta name="language" content="pt-BR" />
+          <meta httpEquiv="cache-control" content="no-cache" />
+          <meta httpEquiv="pragma" content="no-cache" />
+          <meta name="language" content="en" />
           <meta name="rating" content="general" />
-          <link rel="canonical" href="https://www.evander.com.br/" />
+          <link rel="canonical" href="#" />
 
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
@@ -69,9 +71,9 @@ export default class MyDocument extends Document {
             rel="stylesheet"
           />
 
-          <meta property="og:url" content="https://www.evander.com.br/" />
+          <meta property="og:url" content="#" />
           <meta property="og:type" content="website" />
-          <meta property="og:site_name" content="Evander Inácio" />
+          <meta property="og:site_name" content="Yukesh Chandran" />
           <meta property="og:title" content={meta.title} />
           <meta property="og:description" content={meta.description} />
           <meta property="og:image" content={meta.image} />
@@ -87,6 +89,6 @@ export default class MyDocument extends Document {
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
